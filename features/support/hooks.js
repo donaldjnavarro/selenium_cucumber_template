1008b279b416f7fa90239ee8ae0ec86150a1fc9a
Donny Navarro <donaldjnavarro@gmail.com>
2025-06-18 10:03:37 -0400

    Save screenshots on failures

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -3,6 +3,8 @@
  */
 const { After, Before } = require('@cucumber/cucumber');
 const { startBrowser } = require('../../utils/browser');
+const fs = require('fs');
+const path = require('path');
 
 // BeforeAll(async function () {
 //   console.log(`BeforeAll`);
@@ -18,6 +20,21 @@ After(async function () {
   /** Teardown browser if one is open */
   if (this.driver) {
     this.driver.quit();
+  }
+});
+
+After(async function (scenario) {
+  if (scenario.result.status === 'FAILED' && this.driver) {
+    const screenshot = await this.driver.takeScreenshot();
+
+    const dir = path.join(process.cwd(), 'reports/screenshots');
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
+    const filepath = path.join(dir, `${scenario.pickle.name.replace(/\s+/g, '_')}.png`);
+    fs.writeFileSync(filepath, screenshot, 'base64');
+    console.log(`📸 Screenshot saved to ${filepath}`);
   }
 });
 
